4c2f98c69313775e7528d3dfcdfa62b05916e300
kimdohun0104 <dikolight203@gmail.com>
2019-11-20 17:01:41 +0900

    [UPDATE] Select category view with CustomView 카테고리를 선택하는 레이아웃은 4곳에서 중복되어 하나의 커스텀뷰로 빼줌

--- app/src/main/java/com/dsm/iriscalendar/ui/modifyFixedSchedule/ModifyFixedScheduleActivity.java
+++ app/src/main/java/com/dsm/iriscalendar/ui/modifyFixedSchedule/ModifyFixedScheduleActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 import com.dsm.iriscalendar.R;
 import com.dsm.iriscalendar.base.BaseActivity;
 import com.dsm.iriscalendar.data.model.Category;
+import com.dsm.iriscalendar.ui.custom.CategoryView;
 
 import javax.inject.Inject;
 
@@ -18,28 +19,9 @@ import butterknife.ButterKnife;
 
 public class ModifyFixedScheduleActivity extends BaseActivity implements ModifyFixedScheduleContract.View {
 
-    @BindView(R.id.view_first)
-    View viewFirst;
+    @BindView(R.id.cv_modify_fixed_schedule) CategoryView cvModifyFixedSchedule;
 
-    @BindView(R.id.view_second)
-    View viewSecond;
-
-    @BindView(R.id.view_third)
-    View viewThird;
-
-    @BindView(R.id.view_fourth)
-    View viewFourth;
-
-    @BindAnim(R.anim.anim_big)
-    Animation animBig;
-
-    @BindAnim(R.anim.anim_small)
-    Animation animSmall;
-
-    private int upState;
-
-    @Inject
-    ModifyFixedScheduleContract.Presenter presenter;
+    @Inject ModifyFixedScheduleContract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,54 +29,13 @@ public class ModifyFixedScheduleActivity extends BaseActivity implements ModifyF
         setContentView(R.layout.activity_modify_fixed_schedule);
         ButterKnife.bind(this);
         presenter.createView(this);
-        animBig.setFillAfter(true);
 
         presenter.getCategory();
     }
 
-    public void onClickCategory(View v) {
-        int clickedView = v.getId();
-
-        if (clickedView == upState) {
-            return;
-        }
-
-        switch (upState) {
-            case R.id.view_first:
-                viewFirst.startAnimation(animSmall);
-                break;
-            case R.id.view_second:
-                viewSecond.startAnimation(animSmall);
-                break;
-            case R.id.view_third:
-                viewThird.startAnimation(animSmall);
-                break;
-            case R.id.view_fourth:
-                viewFourth.startAnimation(animSmall);
-                break;
-        }
-
-        if (clickedView == R.id.view_first) {
-            viewFirst.startAnimation(animBig);
-            upState = R.id.view_first;
-        } else if (clickedView == R.id.view_second) {
-            viewSecond.startAnimation(animBig);
-            upState = R.id.view_second;
-        } else if (clickedView == R.id.view_third) {
-            viewThird.startAnimation(animBig);
-            upState = R.id.view_third;
-        } else if (clickedView == R.id.view_fourth) {
-            viewFourth.startAnimation(animBig);
-            upState = R.id.view_fourth;
-        }
-    }
-
     @Override
     public void setCategory(Category category) {
-        ((TextView) findViewById(R.id.tv_purple)).setText(category.getPurple());
-        ((TextView) findViewById(R.id.tv_blue)).setText(category.getBlue());
-        ((TextView) findViewById(R.id.tv_pink)).setText(category.getPink());
-        ((TextView) findViewById(R.id.tv_orange)).setText(category.getOrange());
+        cvModifyFixedSchedule.setCategory(category);
     }
 
     @Override
--- app/src/main/java/com/dsm/iriscalendar/ui/modifySchedule/ModifyScheduleActivity.java
+++ app/src/main/java/com/dsm/iriscalendar/ui/modifySchedule/ModifyScheduleActivity.java
@@ -1,16 +1,14 @@
 package com.dsm.iriscalendar.ui.modifySchedule;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dsm.iriscalendar.R;
 import com.dsm.iriscalendar.base.BaseActivity;
 import com.dsm.iriscalendar.data.model.CalendarSchedule;
 import com.dsm.iriscalendar.data.model.Category;
+import com.dsm.iriscalendar.ui.custom.CategoryView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,52 +18,27 @@ import java.util.Objects;
 
 import javax.inject.Inject;
 
-import butterknife.BindAnim;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ModifyScheduleActivity extends BaseActivity implements ModifyScheduleContract.View {
 
-    @BindView(R.id.view_first)
-    View viewFirst;
+    @BindView(R.id.et_todo) EditText etTodo;
 
-    @BindView(R.id.view_second)
-    View viewSecond;
+    @BindView(R.id.et_year) EditText etYear;
 
-    @BindView(R.id.view_third)
-    View viewThird;
+    @BindView(R.id.et_month) EditText etMonth;
 
-    @BindView(R.id.view_fourth)
-    View viewFourth;
+    @BindView(R.id.et_date) EditText etDate;
 
-    @BindAnim(R.anim.anim_big)
-    Animation animBig;
+    @BindView(R.id.et_required_time) EditText etRequiredTime;
 
-    @BindAnim(R.anim.anim_small)
-    Animation animSmall;
-
-    @BindView(R.id.et_todo)
-    EditText etTodo;
-
-    @BindView(R.id.et_year)
-    EditText etYear;
-
-    @BindView(R.id.et_month)
-    EditText etMonth;
-
-    @BindView(R.id.et_date)
-    EditText etDate;
-
-    @BindView(R.id.et_required_time)
-    EditText etRequiredTime;
-
-    private int upState;
+    @BindView(R.id.cv_modify_schedule) CategoryView cvModifySchedule;
 
     private String startTime = "";
     private String endTime = "";
 
-    @Inject
-    ModifyScheduleContract.Presenter presenter;
+    @Inject ModifyScheduleContract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +46,6 @@ public class ModifyScheduleActivity extends BaseActivity implements ModifySchedu
         setContentView(R.layout.activity_modify_schedule);
         ButterKnife.bind(this);
         presenter.createView(this);
-        animBig.setFillAfter(true);
 
         findViewById(R.id.tv_cancel).setOnClickListener(v -> finish());
 
@@ -84,21 +56,7 @@ public class ModifyScheduleActivity extends BaseActivity implements ModifySchedu
 
         findViewById(R.id.tv_complete).setOnClickListener(v -> {
             String calendarName = etTodo.getText().toString().trim();
-            String category = "";
-            switch (upState) {
-                case R.id.view_first:
-                    category = "purple";
-                    break;
-                case R.id.view_second:
-                    category = "blue";
-                    break;
-                case R.id.view_third:
-                    category = "pink";
-                    break;
-                case R.id.view_fourth:
-                    category = "orange";
-                    break;
-            }
+            String category = cvModifySchedule.getCategory();
             SimpleDateFormat format =  new SimpleDateFormat("HH:mm", Locale.KOREA);
             String end = "";
             try {
@@ -110,70 +68,14 @@ public class ModifyScheduleActivity extends BaseActivity implements ModifySchedu
         });
     }
 
-    public void onClickCategory(View v) {
-        int clickedView = v.getId();
-
-        if (clickedView == upState) {
-            return;
-        }
-
-        switch (upState) {
-            case R.id.view_first:
-                viewFirst.startAnimation(animSmall);
-                break;
-            case R.id.view_second:
-                viewSecond.startAnimation(animSmall);
-                break;
-            case R.id.view_third:
-                viewThird.startAnimation(animSmall);
-                break;
-            case R.id.view_fourth:
-                viewFourth.startAnimation(animSmall);
-                break;
-        }
-
-        if (clickedView == R.id.view_first) {
-            viewFirst.startAnimation(animBig);
-            upState = R.id.view_first;
-        } else if (clickedView == R.id.view_second) {
-            viewSecond.startAnimation(animBig);
-            upState = R.id.view_second;
-        } else if (clickedView == R.id.view_third) {
-            viewThird.startAnimation(animBig);
-            upState = R.id.view_third;
-        } else if (clickedView == R.id.view_fourth) {
-            viewFourth.startAnimation(animBig);
-            upState = R.id.view_fourth;
-        }
-    }
-
     @Override
     public void setCategory(Category category) {
-        ((TextView) findViewById(R.id.tv_blue)).setText(category.getBlue());
-        ((TextView) findViewById(R.id.tv_purple)).setText(category.getPurple());
-        ((TextView) findViewById(R.id.tv_orange)).setText(category.getOrange());
-        ((TextView) findViewById(R.id.tv_pink)).setText(category.getPink());
+        cvModifySchedule.setCategory(category);
     }
 
     @Override
     public void setSchedule(CalendarSchedule schedule) {
-        switch (schedule.getCategory()) {
-            case "purple":
-                upState = R.id.view_first;
-                viewFirst.startAnimation(animBig);
-                break;
-            case "blue":
-                upState = R.id.view_second;
-                viewSecond.startAnimation(animBig);
-                break;
-            case "pink":
-                upState = R.id.view_third;
-                viewThird.startAnimation(animBig);
-                break;
-            case "orange":
-                upState = R.id.view_fourth;
-                viewFourth.startAnimation(animBig);
-        }
+        cvModifySchedule.selectCategory(schedule.getCategory());
 
         String endDate = schedule.getEndTime().split(" ")[0];
         etYear.setText(endDate.split("-")[0]);
